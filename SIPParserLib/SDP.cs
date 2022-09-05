c5bc6c1bf34029a8f18e8fb0cf1f9ce2713dd288
dfgs <dfgs83@gmail.com>
2022-09-05 17:16:06 +0200

    Added codec search in SDP

--- SIPParserLib/SDP.cs
+++ SIPParserLib/SDP.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SIPParserLib
 {
 	public class SDP
 	{
+		private Regex mediaListRegex = new Regex(@"(?<Index>\d+) *");
+		private Regex codecRegex = new Regex(@"(?<Index>\d+) (?<Name>.*)");
+
 		private SDPField[] fields;
 		public IEnumerable<SDPField> Fields
 		{
@@ -34,6 +38,35 @@ namespace SIPParserLib
 		{
 			return fields.OfType<T>();
 		}
+		public IEnumerable<int> GetMediaIndices()
+		{
+			MatchCollection matches;
+			MediaField? mediaField;
+
+			mediaField = GetField<MediaField>();
+			if (mediaField == null) yield break;
+			matches= mediaListRegex.Matches(mediaField.Format);
+			
+			foreach(Match match in matches)
+			{
+				yield return int.Parse(match.Groups["Index"].Value);
+			}
+		}
+		public string? GetCodec(int MediaIndex)
+		{
+			Match match;
+			string index;
+
+			index = MediaIndex.ToString();
+			foreach(AttributeField attributeField in GetFields<AttributeField>().Where(item=>item.Value.Name=="rtpmap"))
+			{
+				match = codecRegex.Match(attributeField?.Value.Value??"");
+				if (!match.Success) continue;
+				if (match.Groups["Index"].Value == index) return match.Groups["Name"].Value;
+			}
+			return null;
+		}
+
 
 
 	}
